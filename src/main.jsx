1a33bb89c6dca2b58df4cfc62d07b5dd737ce53e
lion9797@naver.com <lion9797@naver.com>
2025-06-05 18:01:32 +0900

    535

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import About from "./pages/About.jsx";  // 필요하면 페이지 생성
 // import Contact from "./pages/Contact.jsx"; // Contact 페이지 만들면 import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/about" element={<About />} />
       {/* <Route path="/contact" element={<Contact />} />  Contact 페이지 만들면 활성화 */}
     </Routes>
-  </BrowserRouter >
+  </HashRouter>
 );
 
 
