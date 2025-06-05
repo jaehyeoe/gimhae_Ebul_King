246e9973cf6f2439332c23be9c2f2e039c795a0e
lion9797@naver.com <lion9797@naver.com>
2025-06-05 16:42:06 +0900

    go

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx 또는 App.jsx에서 라우터 설정
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import About from "./pages/About.jsx"; // 예시로 추가한 페이지
+import Contact from "./pages/Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>회사 소개</h1>
+      <p>김해 이불왕국은 프리미엄 침구 전문 브랜드입니다.</p>
+    </div>
+  );
+}
